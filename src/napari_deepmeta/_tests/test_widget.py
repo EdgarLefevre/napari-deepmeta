4a987ddbe20c2fbbbcb3b30e20860eeb6f48d7f3
Edgar <lefevreedg@gmail.com>
2022-06-10 10:51:16 +0200

    :bug: rewrite tests

--- src/napari_deepmeta/_tests/test_widget.py
+++ src/napari_deepmeta/_tests/test_widget.py
@@ -1,17 +1,17 @@
 import numpy as np
 
-from napari_deepmeta import ExampleQWidget, example_magic_widget
+from napari_deepmeta import DeepmetaDemoWidget, DeepmetaWidget
 
 
 # make_napari_viewer is a pytest fixture that returns a napari viewer object
 # capsys is a pytest fixture that captures stdout and stderr output streams
-def test_example_q_widget(make_napari_viewer, capsys):
+def test_deepmeta_widget(make_napari_viewer, capsys):
     # make viewer and add an image layer using our fixture
     viewer = make_napari_viewer()
     viewer.add_image(np.random.random((100, 100)))
 
     # create our widget, passing in the viewer
-    my_widget = ExampleQWidget(viewer)
+    my_widget = DeepmetaWidget(viewer)
 
     # call our widget method
     my_widget._on_click()
@@ -21,16 +21,17 @@ def test_example_q_widget(make_napari_viewer, capsys):
     assert captured.out == "napari has 1 layers\n"
 
 
-def test_example_magic_widget(make_napari_viewer, capsys):
+def test_demo_widget(make_napari_viewer, capsys):
+    # make viewer and add an image layer using our fixture
     viewer = make_napari_viewer()
-    layer = viewer.add_image(np.random.random((100, 100)))
+    viewer.add_image(np.random.random((100, 100)))
 
-    # this time, our widget will be a MagicFactory or FunctionGui instance
-    my_widget = example_magic_widget()
+    # create our widget, passing in the viewer
+    my_widget = DeepmetaDemoWidget(viewer)
 
-    # if we "call" this object, it'll execute our function
-    my_widget(viewer.layers[0])
+    # call our widget method
+    my_widget._on_click()
 
     # read captured output and check that it's as we expected
     captured = capsys.readouterr()
-    assert captured.out == f"you have selected {layer}\n"
+    assert captured.out == "napari has 1 layers\n"
