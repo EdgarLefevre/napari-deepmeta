03b3c392c6a641e2fe5655ca67483b552c1ca280
Edgar <lefevreedg@gmail.com>
2022-06-10 14:48:22 +0200

    :construction: add tests

--- src/napari_deepmeta/_tests/test_widget.py
+++ src/napari_deepmeta/_tests/test_widget.py
@@ -31,3 +31,26 @@ def test_demo_widget(make_napari_viewer, capsys):
     my_widget._on_click()
 
     assert len(viewer.layers) == 4
+
+
+def test_add_z():
+    import napari_deepmeta.deepmeta_functions as df
+
+    arr = np.array([[0, 1], [0, 1]])
+    assert (df.add_z(arr, 3) == np.array([[3, 0, 1], [3, 0, 1]])).all()
+
+
+def test_dilate_and_erode():
+    import napari_deepmeta.deepmeta_functions as df
+
+    img = np.zeros((128, 128))
+    res = df.dilate_and_erode(img)
+    assert np.shape(img) == np.shape(res)
+
+
+def test_contrast():
+    import napari_deepmeta.deepmeta_functions as df
+
+    seg = np.ones((2, 128, 128))
+    res = df.contrast_and_reshape(seg)
+    assert np.shape(res) == (2, 1, 128, 128)
